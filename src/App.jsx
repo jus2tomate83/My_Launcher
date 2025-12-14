72856396e768e9bbd64b5a6faef6f083bc3b1493
jus2tomate83 <121179070+jus2tomate83@users.noreply.github.com>
2025-12-14 12:28:31 +0100

    fix: add router basename for gh-pages support

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Admin from './pages/Admin';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
